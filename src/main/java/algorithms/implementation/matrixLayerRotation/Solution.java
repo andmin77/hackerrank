6dde099aeb4358779e293cb423e80aca37826545
mibesoft <andreaminnucci77@gmail.com>
2018-09-03 17:25:34 +0200

    commit

--- src/main/java/algorithms/implementation/matrixLayerRotation/Solution.java
+++ src/main/java/algorithms/implementation/matrixLayerRotation/Solution.java
@@ -4,56 +4,88 @@ import java.util.*;
 
 public class Solution {
    
-    public static void main(String[] args) {
+    private static enum Orientation {
+        North,
+        East,
+        South,
+        West;
+    }
+
+    public static void main(String[] args) {            
         Scanner scanner = new Scanner(System.in);
         int M = scanner.nextInt();
         int N = scanner.nextInt();
         int R = scanner.nextInt();
-        int[][] matrix = new int[M][N];                
+        int[][] matrix = new int[M][N];
         for ( int row = 0; row < M; row++ ) {
             for ( int col = 0; col < N; col++ ) {
                 matrix[row][col] = scanner.nextInt();
             }
         }
-        R = R % ( 2 * (M + N) );
         int middle = Math.min(M, N) / 2;
-        for ( int r = 0; r < R; r++ ) {
-            for ( int index = 0; index < middle; index++ ) {
-                int temp = matrix[index][index];
-                for ( int col = index; col < N - index - 1; col++ ) {
-                    matrix[index][col] = matrix[index][col + 1];
-                }            
-                for ( int row = index; row < M - index - 1; row++ ) {
-                    matrix[row][N - index - 1] = matrix[row + 1][N - index - 1];
-                }
-                for ( int col = N - index - 1; col > index; col-- ) {
-                    matrix[M - index - 1][col] = matrix[M - index - 1][col - 1];
-                } 
-                for ( int row = M - index - 1; row > index; row-- ) {
-                    matrix[row][index] = matrix[row - 1][index];
-                }
-                matrix[index + 1][index] = temp;
+        List<List<Integer>> data = new ArrayList<>();
+        for ( int index = 0; index < middle; index++ ) {
+            List<Integer> e = new ArrayList<>();
+            for ( int col = index; col < N - index; col++ ) {
+                e.add( matrix[index][col] );
+            }            
+            for ( int row = index + 1; row < M - index; row++ ) {
+                e.add( matrix[row][N - index - 1] );
             }
-        }
-        
-        /*
-        String[] rcn = scanner.nextLine().split(" ");
-        int M = Integer.parseInt(rcn[0]);
-        int N = Integer.parseInt(rcn[1]);
-        int R = Integer.parseInt(rcn[2]);
-        int[][] matrix = new int[M][N];                
-        for ( int row = 0; row < M; row++ ) {
-            String gridItem = scanner.nextLine();
-            for ( int col = 0; col < N; col++ ) {
-                matrix[row][col] = Integer.parseInt( "" + gridItem.charAt(col) );
+            for ( int col = N - index - 1; col > index; col-- ) {
+                e.add( matrix[M - index - 1][col - 1] );
+            } 
+            for ( int row = M - index - 2; row > index; row-- ) {
+                e.add( matrix[row][index] );
             }
+            data.add( e );            
+        }        
+        int[][] output = new int[M][N];
+        int index = 0;
+        for ( List<Integer> e : data ) {
+            int rotation = R % e.size();
+            int[] t_array = new int[e.size()];
+            for ( int i = 0; i < t_array.length; i++ ) {
+                if ( i + rotation < e.size() ) {
+                    t_array[i] = e.get(i + rotation);
+                } else {
+                    t_array[i] = e.get( rotation - ( e.size() - i) );
+                }
+            }
+            Orientation orientation = Orientation.North;
+            int row = index, col = index;
+            for ( int i = 0; i < t_array.length; i++ ) {
+                output[row][col] = t_array[i];
+                switch ( orientation ) {
+                    case North:
+                        col++;
+                        if ( col >= N - index - 1 ) {
+                            orientation = Orientation.East;
+                        }
+                        break;
+                    case East:
+                        row++;
+                        if ( row >= M - index - 1 ) {
+                            orientation = Orientation.South;
+                        }
+                        break;
+                    case South:
+                        col--;
+                        if ( col <= index ) {
+                            orientation = Orientation.West;
+                        }
+                        break;
+                    case West:
+                        row--;
+                        break;
+                }
+            }
+            index++;
         }
-        */
-        
         for ( int row = 0; row < M; row++ ) {
             String r = "";
             for ( int col = 0; col < N; col++ ) {
-                r += matrix[row][col] + " ";
+                r += output[row][col] + " ";
             }
             r = r.substring(0, r.length() - 1);
             System.out.println( r );
